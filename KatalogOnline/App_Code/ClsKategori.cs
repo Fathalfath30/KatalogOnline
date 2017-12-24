663c1dbd36f1faaac9b83d2e5e7f5d87f6d21208
Fathalfath30 <fathalfath30@gmail.com>
2017-12-24 17:34:09 +0700

    Update UAS

---
+++ KatalogOnline/App_Code/ClsKategori.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Data;
+using System.Configuration;
+using System.Linq;
+using System.Web;
+using System.Web.Security;
+using System.Web.UI;
+using System.Web.UI.HtmlControls;
+using System.Web.UI.WebControls;
+using System.Web.UI.WebControls.WebParts;
+using System.Xml.Linq;
+using System.Data.SqlClient;
+using System.Web.Configuration;
+using System.Collections.Generic;
+
+namespace KatalogOnline {
+    public class ClsKategori : KatalogOnlineModel<ClsKategori> {
+        public string auto_number() {
+            throw new NotImplementedException();
+        }
+
+        public List<ClsKategori> cari_data() {
+            throw new NotImplementedException();
+        }
+
+        public bool hapus_data() {
+            throw new NotImplementedException();
+        }
+
+        public bool simpan_data() {
+            throw new NotImplementedException();
+        }
+
+        public List<ClsKategori> tampil_data() {
+            throw new NotImplementedException();
+        }
+
+        public bool update_data() {
+            throw new NotImplementedException();
+        }
+    }
+}
--- KatalogOnline/App_Code/KatalogOnlineModel.cs
+++ KatalogOnline/App_Code/KatalogOnlineModel.cs
@@ -1,17 +1,12 @@
-﻿using System;
-using System.Web;
-using System.Web.Services;
-using System.Web.Services.Protocols;
-using System.ComponentModel;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 
 namespace KatalogOnline {
     public interface KatalogOnlineModel<T> {
-        List<T> tampil_data();
-        List<T> cari_data();
-        bool simpan_data();
-        bool hapus_data();
-        bool update_data();
         string auto_number();
+        List<T> cari_data();
+        bool hapus_data();
+        bool simpan_data();
+        List<T> tampil_data();
+        bool update_data();
     }
 }
